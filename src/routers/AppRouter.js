b3a93f8023ae28ef15382be44e87e6ccf77b973e
Santiago Pegels <santiagopegels@gmail.com>
2021-06-10 10:10:06 -0300

    add router and screens

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+import { Navbar } from '../components/ui/NavBar';
+import { LoginScreen } from '../components/login/LoginScreen';
+import { MarvelScreen } from '../components/marvel/MarvelScreen';
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <div>
+                <Navbar />
+
+                <Switch>
+                    <Route exact path="/login" component={LoginScreen} />
+                    <Route exact path="/" component={MarvelScreen} />
+                </Switch>
+            </div>
+        </Router>
+    )
+}
