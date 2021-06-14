73d1789cce53d166cd33308f3e44a7fce1c97082
Santiago Pegels <santiagopegels@gmail.com>
2021-06-14 11:16:22 -0300

    Add private and public route

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../auth/AuthContext'
+import { types } from '../../types/types'
 
 export const LoginScreen = ({history}) => {
 
+    const {dispatch} = useContext(AuthContext)
+
     const handleLogin = () => {
-        history.replace('/')
+
+        const lastPath = localStorage.getItem('lastPath') || '/'
+
+        dispatch({
+            type: types.login,
+            payload: {
+                name: 'UserTest'
+            }
+        })
+        history.replace(lastPath)
     }
 
     return (
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {
     BrowserRouter as Router,
     Switch,
@@ -6,15 +6,31 @@ import {
 } from "react-router-dom";
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
+import { PrivateRoute } from './PrivateRoute';
+import { AuthContext } from '../auth/AuthContext';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
+
+    const { user } = useContext(AuthContext)
+
     return (
         <Router>
             <div>
 
                 <Switch>
-                    <Route exact path="/login" component={LoginScreen} />
-                    <Route path="/" component={DashboardRoutes} />
+                    <PublicRoute
+                        exact
+                        path="/login"
+                        component={LoginScreen}
+                        isAuthenticated={user.logged}
+                    />
+
+                    <PrivateRoute
+                        path="/"
+                        component={DashboardRoutes}
+                        isAuthenticated={user.logged}
+                    />
                 </Switch>
             </div>
         </Router>
